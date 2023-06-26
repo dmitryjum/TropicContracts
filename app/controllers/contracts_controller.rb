3bea0d0fb899eaa09b4b25cea122de5c91808a73
Dmitry Jum <xfiler@ymail.com>
2023-06-25 23:21:00 -0700

    WIP: modal shows up with turbo_stream_tag, but more work needed

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -2,4 +2,8 @@ class ContractsController < ApplicationController
   def index
     @contracts = Contract.includes(:contract_owner)
   end
+
+  def new
+    @contract = Contract.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "contracts#index"
 
-  resources :contracts, only: [:index]
+  resources :contracts, only: [:index, :new]
 end
