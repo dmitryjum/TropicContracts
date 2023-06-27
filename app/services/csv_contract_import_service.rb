ad447955f884420fc3f31f47372d6ba1a2b5564d
Dmitry Jum <xfiler@ymail.com>
2023-06-27 13:54:07 -0700

    WIP: saves or updates contract owners from CSV, contracts upsert needds work

--- app/services/csv_contract_import_service.rb
+++ app/services/csv_contract_import_service.rb
@@ -1,27 +1,26 @@
 class CsvContractImportService
+  require 'csv'
   def initialize(file)
     @opened_file = File.open(file)
   end
 
   def call
-    options = {
-        header: :use,
-        parse_empty_fields_as: :nil
-      }
-    Rcsv.parse(@opened_file, options).each_slice(500) do |batch|
+    table = CSV.parse(@opened_file, headers: true)
+    table.each_slice(500) do |batch|
       emails = get_unique_emails(batch)
       owners = create_and_return_owners(emails)
+      contracts = upsert_contracts(owners, batch)
     end
   end
-    
+  
   private
   
   def get_unique_emails(batch)
-    batch.map {|row| row[0]}.compact.uniq
+    batch.map {|row| row["Contract Owner"]}.compact.uniq
   end
-
+  
   def create_and_return_owners(emails)
-    owners_array = emails.map do |email|
+    owners_hashes = emails.map do |email|
       split_email = email.split(/\.|@/)
       {
         first_name: split_email[0],
@@ -29,6 +28,37 @@ class CsvContractImportService
         email: email
       }
     end
+
+    ContractOwner.upsert_all(
+      owners_hashes,
+      unique_by: [:email, :index_contract_owners_on_email],
+      returning: [:email, :id],
+      update_only: [:first_name, :last_name]
+    )
+  end
+  
+  def upsert_contracts(owners, batch)
+    contract_hashes = batch.map do |row|
+      time = Time.current
+      {
+        external_contract_id: row["External Contract ID"],
+        name: row["Contract Name"],
+        start_date: row["Start Date"],
+        end_date: row["End Date"],
+        value_cents: row["Contract Value"].try(:gsub, /\D/, '').try(:to_i),
+        supplier: row["Supplier"],
+        updated_at: time,
+        created_at: time,
+        contract_owner_id: owners.rows.to_h[row["Contract Owner"]]
+      }
+    end
+
+    contracts = Contract.upsert_all(
+      contract_hashes,
+      unique_by: [:external_contract_id, :index_contracts_on_external_contract_id],
+      returning: [:name, :start_date, :end_date, :value_cents, :supplier, :contract_owner_id]
+      # update_only: [:name, :start_date, :end_date, :value_cents, :supplier, :contract_owner_id]
+    )
     debugger
   end
 end
