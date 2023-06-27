fd14a17f35a724e7036d66e1ac858a75a25f7f33
Dmitry Jum <xfiler@ymail.com>
2023-06-26 17:26:10 -0700

    refactors contract row supplier link into a view component method

--- app/components/contract_row_component.rb
+++ app/components/contract_row_component.rb
@@ -7,4 +7,13 @@ class ContractRowComponent < ViewComponent::Base
     @contract = contract
     @supplier_name = supplier_name
   end
+
+  def supplier_view_link
+    tag.div class: "table-cell border-t border-gray-300 p-4 pl-8 #{hidden_if(supplier_name)}" do
+      link_to contract.supplier,
+              supplier_contracts_path(supplier_name: contract.supplier),
+              class: "link-text",
+              data: { turbo_frame: "table_container" } if contract.supplier.present?
+    end
+  end
 end
