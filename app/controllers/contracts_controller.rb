81d4ca814b0261e954f508524339582f2407320f
Dmitry Jum <xfiler@ymail.com>
2023-06-28 04:52:41 -0700

    handles some unexpected error in the import_csv acdtion

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -13,17 +13,22 @@ class ContractsController < ApplicationController
   end
 
   def import_csv
-    return redirect_to request.referer, notice: 'No file added' if params[:file].nil?
-    return redirect_to request.referer, notice: 'Only CSV files allowed' unless params[:file].content_type == 'text/csv'
-    import_service = CsvContractImportService.new(params[:file])
-    import_service.call
-    @contracts = Contract.includes(:contract_owner)
-    @updated_or_created_counter = import_service.updated_contracts_counter
-    @invalid_records = import_service.invalid_contract_instances
-    flash.now[:notice] = "#{@updated_or_created_counter} records have been created or updated successfuly" if @updated_or_created_counter > 0
-    flash.now[:alert] = {invalid_records: @invalid_records} if @invalid_records.size > 0
-    respond_to do |format|
-      format.turbo_stream
+    begin
+      return redirect_to request.referer, notice: 'No file added' if params[:file].nil?
+      return redirect_to request.referer, notice: 'Only CSV files allowed' unless params[:file].content_type == 'text/csv'
+      import_service = CsvContractImportService.new(params[:file])
+      import_service.call
+      @contracts = Contract.includes(:contract_owner)
+      @updated_or_created_counter = import_service.updated_contracts_counter
+      @invalid_records = import_service.invalid_contract_instances
+      flash.now[:notice] = "#{@updated_or_created_counter} records have been created or updated successfuly" if @updated_or_created_counter > 0
+      flash.now[:alert] = {invalid_records: @invalid_records} if @invalid_records.size > 0
+      respond_to do |format|
+        format.turbo_stream
+      end
+    rescue => e
+      #but things like these should be caught with middlware error logging service and reported (HoneyBadger, Rollbar, etc)
+      render :index, status: :unprocessable_entity
     end
   end
 end
