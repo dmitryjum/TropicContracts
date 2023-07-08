32962b785eec1c0ef99aa810792b8ecb5f55ca99
Dmitry Jum <xfiler@ymail.com>
2023-07-07 22:05:25 -0700

    WIP: ContractsContainerComponent passes with supplier name example

--- spec/components/contracts_container_component_spec.rb
+++ spec/components/contracts_container_component_spec.rb
@@ -4,22 +4,20 @@ RSpec.describe ContractsContainerComponent, type: :component do
   context "with contracts and supplier name" do
     it "renders the component with the supplied data" do
       contracts = [double("Contract")]
-      supplier_name = "ACME Corp"
-      allow_any_instance_of(ContractRowComponent).to receive(:render_in).and_return("Contract Row Rendered")
-      rendered_component = render_inline(described_class.new(contracts: contracts))
+      contract_row_stub_text = "Contract Row Rendered"
+      allow_any_instance_of(ContractRowComponent).to receive(:render_in).and_return(contract_row_stub_text)
+      rendered_component = render_inline(described_class.new(contracts: contracts, supplier_name: "ACME Corp"))
 
       expect(rendered_component).to have_selector(".card-container")
-      expect(rendered_component).to have_selector("#contracts")
+      expect(rendered_component).to have_selector("#contracts", text: contract_row_stub_text)
 
       expect(rendered_component).to have_selector(".table-cell", text: "Contract Owner")
-      expect(rendered_component).to have_selector(".table-cell", text: "Supplier")
       expect(rendered_component).to have_selector(".table-cell", text: "Contract Name")
       expect(rendered_component).to have_selector(".table-cell", text: "Start Date")
       expect(rendered_component).to have_selector(".table-cell", text: "End Date")
       expect(rendered_component).to have_selector(".table-cell", text: "Contract Value")
-
+      expect(rendered_component).to have_selector(".hidden", text: "Supplier")
       expect(rendered_component).to have_selector(".table-row", count: 1)
-      expect(rendered_component).to have_content("ACME Corp")
     end
   end
 end
